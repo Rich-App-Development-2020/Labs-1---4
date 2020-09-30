0f716892111adf24d1db8b2048cfc89b53f483c8
Johnathon Crawford <47326832+Kodi125@users.noreply.github.com>
2020-09-30 21:19:56 +0100

    up to Question 15 completed

--- Rad3012020Week1Lab1/ProductModel.cs
+++ Rad3012020Week1Lab1/ProductModel.cs
@@ -10,12 +10,34 @@ namespace Rad3012020Week1Lab1
     {
         public List<Product> products = new List<Product>
         {
-            new Product {                ProductID = 1, description = "9\" Nails", QuantityInStock = 200, UnitPrice = 0.10f, CategoryID = 1},
-            new Product {                ProductID = 2, description = "9\" Bolts", QuantityInStock = 120, UnitPrice = 0.20f, CategoryID = 1},
-            new Product {                ProductID = 3, description = "Chimney Hoover", QuantityInStock = 10, UnitPrice = 100.50f,CategoryID = 2},
-            new Product {                ProductID = 4, description = "Washing Machine", QuantityInStock = 7, UnitPrice = 399.99f , CategoryID = 2},
-    };
-}
+            new Product { ProductID = 1, description = "9\" Nails", QuantityInStock = 200, UnitPrice = 0.10f, CategoryID = 1},
+            new Product { ProductID = 2, description = "9\" Bolts", QuantityInStock = 120, UnitPrice = 0.20f, CategoryID = 1},
+            new Product { ProductID = 3, description = "Chimney Hoover", QuantityInStock = 10, UnitPrice = 100.50f,CategoryID = 2},
+            new Product { ProductID = 4, description = "Washing Machine", QuantityInStock = 7, UnitPrice = 399.99f , CategoryID = 2},
+        };
+
+        public List<Category> categories = new List<Category>
+        {
+            new Category { Id = 1, description = "Hardware"},
+            new Category { Id = 2, description = "Electrical Appliances"},
+            
+        };
+
+        public List<SupplierProduct> supplierProducts = new List<SupplierProduct>
+        {
+            new SupplierProduct { SupplierID = 1, ProductID = 1 , DateFirstSupplied = new DateTime(2012, 12, 12)},
+            new SupplierProduct { SupplierID = 1, ProductID = 2 , DateFirstSupplied = new DateTime(2013, 06, 01)},
+            new SupplierProduct { SupplierID = 2, ProductID = 3 , DateFirstSupplied = new DateTime(2017, 09, 09)},
+            new SupplierProduct { SupplierID = 2, ProductID = 4 , DateFirstSupplied = new DateTime(2017, 10, 09)},
+        };
+
+        public List<Supplier> suppliers = new List<Supplier>
+        {
+            new Supplier { SupplierID = 1, SupplierName = "Parts 1" , AddressLine1 = "Addr 11", AddressLine2 = "Addr 21"},
+            new Supplier { SupplierID = 1, SupplierName = "Parts 2" , AddressLine1 = "Addr 11", AddressLine2 = "Addr 21"},
+        };
+
+    }
     
     public class Product
     {
@@ -28,7 +50,7 @@ namespace Rad3012020Week1Lab1
     }
     public class Category
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string description { get; set; }
 
     }
--- Rad3012020Week1Lab1/Program.cs
+++ Rad3012020Week1Lab1/Program.cs
@@ -12,6 +12,74 @@ namespace Rad3012020Week1Lab1
         static void Main(string[] args)
         {
             Activity.Track("Designing Classes Model");
+            Activity.Track("Starting Queries");
+            Console.WriteLine();
+
+            //finish listing the objects
+
+            ProductModel model = new ProductModel();
+
+            //listing all the categories
+            Console.WriteLine("CATEGORIES");
+            Console.WriteLine("------------");
+            foreach (var item in model.categories)
+            {
+                Console.WriteLine("{0}", item.description);
+            }
+            Console.WriteLine("------------");
+            Console.WriteLine();
+
+            //listing all the products
+            Console.WriteLine("PRODUCTS");
+            Console.WriteLine("------------");
+            foreach (var item in model.products)
+            {
+                Console.WriteLine(item.description);
+            }
+            Console.WriteLine("------------");
+            Console.WriteLine();
+
+
+            //listing all the products with a quantity of less than 100
+            Console.WriteLine("PRODUCTS WITH QUANTITY LESS THAN 100");
+            Console.WriteLine("------------");
+            var under100 = model.products
+                                        .Where(p => p.QuantityInStock <= 100)
+                                        .OrderByDescending(p => p.QuantityInStock);
+            foreach (var item in under100)
+            {
+                Console.WriteLine(item.description);
+            }
+            Console.WriteLine("------------");
+            Console.WriteLine();
+
+            //listing all the products together with their total value
+            Console.WriteLine("ALL THE PRODUCTS TOGTHER WITH THEIR TOTAL VALUE");
+            Console.WriteLine("------------");
+            foreach (var item in model.products)
+            {
+                float totalValue = item.QuantityInStock * item.UnitPrice;
+                Console.WriteLine(item.description + "\t\t {0:C}", totalValue);
+            }
+            Console.WriteLine("------------");
+            Console.WriteLine();
+
+            //List all the Products in the Hardware Category
+            Console.WriteLine("ALL THE PRODUCTS IN THE HARDWARE CATEGORY");
+            var hardware = model.products.Where(s => s.CategoryID == 1);
+            foreach (var item in hardware)
+            {
+                Console.WriteLine(item.description);
+            }
+
+            //List all the suppliers, their Parts and order it by supplier name
+            //var suppliers = model.suppliers;
+
+            //var idk = (from s in model.supplierProducts
+            //           join p in model.products on s.ProductID equals p.ProductID
+            //           where p.ProductID == s.ProductID
+            //           select( s.)
+
         }
     }
 }
