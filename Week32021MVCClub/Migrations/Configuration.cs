6d7760ad1e4c0976255024ef9d137b027193c609
S00190873 <S00190873@mail.itsligo.ie>
2020-10-15 16:27:07 +0100

    Revert "s00190783 Second Migration and Application"
    This reverts commit e0e38678fb4ce235477fb38f69cd72b82614d906.

--- Week32021MVCClub/Migrations/Configuration.cs
+++ Week32021MVCClub/Migrations/Configuration.cs
@@ -1,12 +1,9 @@
 ﻿namespace Week32021MVCClub.Migrations
 {
-    using Microsoft.AspNet.Identity;
-    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
-    using Week32021MVCClub.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Week32021MVCClub.Models.ApplicationDbContext>
     {
@@ -21,60 +18,6 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
-
-            //sets up two authenticated users
-            var manager =
-                new UserManager<ApplicationUser>(
-                    new UserStore<ApplicationUser>(context));
-
-            var roleManager =
-                new RoleManager<IdentityRole>(
-                    new RoleStore<IdentityRole>(context));
-
-            context.Roles.AddOrUpdate(r => r.Name,
-                new IdentityRole { Name = "Admin" }
-                );
-            context.Roles.AddOrUpdate(r => r.Name,
-                new IdentityRole { Name = "ClubAdmin" }
-                );
-            context.Roles.AddOrUpdate(r => r.Name,
-                new IdentityRole { Name = "member" }
-                );
-
-            PasswordHasher ps = new PasswordHasher();
-
-            context.Users.AddOrUpdate(u => u.UserName,
-                new ApplicationUser
-                {
-                    UserName = "powell.paul@itsligo.ie",
-                    Email = "powell.paul@itsligo.ie",
-                    EmailConfirmed = true,
-                    JoinDate = DateTime.Now,
-                    SecurityStamp = Guid.NewGuid().ToString(),
-                    FirstName = "Paul",
-                    Surname = "Powell",
-                    PasswordHash = ps.HashPassword("Rad3022021$1")
-                });
-
-            context.Users.AddOrUpdate(u => u.UserName,
-                new ApplicationUser
-                {
-                    UserName = "radp2016@outlook.com",
-                    Email = "radp2016@outlook.com",
-                    EmailConfirmed = true,
-                    JoinDate = DateTime.Now,
-                    SecurityStamp = Guid.NewGuid().ToString(),
-                    FirstName = "Rad",
-                    Surname = "Paulner",
-                    PasswordHash = ps.HashPassword("radP2016$1")
-                });
-            context.SaveChanges();
-
-            ApplicationUser admin = manager.FindByEmail("powell.paul@itsligo.ie");
-            if (admin != null)
-            {
-                manager.AddToRoles(admin.Id, new string[] { "Admin", "member", "ClubAdmin" });
-            }
         }
     }
 }
