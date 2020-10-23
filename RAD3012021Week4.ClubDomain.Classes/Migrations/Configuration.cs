fa2e0f633a0f472b384458c11485e34a60483b6e
S00190873 <S00190873@mail.itsligo.ie>
2020-10-23 10:47:20 +0100

    Initial Club Database setup

---
+++ RAD3012021Week4.ClubDomain.Classes/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace RAD3012021Week4.ClubDomain.Classes.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Week32021ClubDomain.Classes.ClubsContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Week32021ClubDomain.Classes.ClubsContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
