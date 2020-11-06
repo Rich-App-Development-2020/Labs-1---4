c2f976094d7045a9c51cf3b99cd5bb0e2a35c0bb
S00190873 <S00190873@mail.itsligo.ie>
2020-11-06 20:08:04 +0000

    Application DB Context Setup

---
+++ RAD3012021Week4.MVCClub/Migrations/202011061950533_Application DB setup.cs
@@ -0,0 +1,16 @@
+﻿namespace RAD3012021Week4.MVCClub.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ApplicationDBsetup : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- RAD3012021Week4.MVCClub/Migrations/Configuration.cs
+++ RAD3012021Week4.MVCClub/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 ﻿namespace RAD3012021Week4.MVCClub.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using RAD3012021Week4.MVCClub.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -18,9 +21,85 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+            
+                //store the usermanager and the context together in a usable variable
+            var manager =
+             new UserManager<ApplicationUser>(
+                 new UserStore<ApplicationUser>(context));
+
+                //store the usermanager and the context together in a usable variable
+            var roleManager =
+                new RoleManager<IdentityRole>(
+                    new RoleStore<IdentityRole>(context));
 
 
-            context.Admon.AddOrUpdate(c => new { c.CourseCode, c.Year }, get_courses().ToArray());
+            #region AddRoleDefinitions
+                //define the different roles by name
+            context.Roles.AddOrUpdate(r => r.Name,
+                new IdentityRole { Name = "Admin" }
+                );
+            context.Roles.AddOrUpdate(r => r.Name,
+                new IdentityRole { Name = "ClubAdmin" }
+                );
+            context.Roles.AddOrUpdate(r => r.Name,
+                new IdentityRole { Name = "member" }
+                );
+            #endregion AddRoleDefinitions
+
+            PasswordHasher ps = new PasswordHasher();
+
+            #region AddRoleDefinitions
+
+                //add new users
+            context.Users.AddOrUpdate(u => u.UserName,
+                new ApplicationUser
+                {
+                    UserName = "powell.paul@itsligo.ie",
+                    Email = "powell.paul@itsligo.ie",
+                    EmailConfirmed = true,
+                    //DateTime constructor = yyyy/mm/dd//hour//min
+                    date_joined = new DateTime(2018, 10, 09, 23, 45, 42),
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    FirstName = "Paul",
+                    Surname = "Powell",
+                    PasswordHash = ps.HashPassword("``")
+                });
+
+            context.Users.AddOrUpdate(u => u.UserName,
+                new ApplicationUser
+                {
+                    UserName = "radp2016@outlook.com",
+                    Email = "radp2016@outlook.com",
+                    EmailConfirmed = true,
+                    date_joined = new DateTime(2018, 10, 09, 23, 45, 42),
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    FirstName = "Rad",
+                    Surname = "Paulner",
+                    PasswordHash = ps.HashPassword("radP2016$1")
+                });
+            context.SaveChanges();
+
+            #endregion AddRoleDefinitions
+
+            #region AddFullAdmin
+            ApplicationUser admin = manager.FindByEmail("powell.paul@itsligo.ie");
+            if (admin != null)
+            {
+                manager.AddToRoles(admin.Id, new string[] { "Admin", "member", "ClubAdmin" });
+            }
+            #endregion AddFullAdmin
+
+            #region AddNormalUser
+            //create a variable to describe the rights of the user, this is just a normal user so I just called it user, 
+            //can call it anything, it's just storing the user that you found by email
+            //you then assign the roles you want to to that variable you created, then update database and a new role and
+            //the ID for the member associated with that role will show in the database
+            ApplicationUser user = manager.FindByEmail("radp2016@outlook.com");
+            if (user != null)
+            {
+                manager.AddToRoles(user.Id, new string[] { "member"});
+            }
+            #endregion AddNormalUser
         }
     }
 }
